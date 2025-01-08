b4276a44d31641848f314ac1c97606dea924cfe0
crispimUchoa <francisco,crispim@aluno.uece.br>
2025-01-08 19:08:59 -0300

    create category model and add in admin page

--- contact/admin.py
+++ contact/admin.py
@@ -2,12 +2,20 @@ from django.contrib import admin
 from contact import models
 
 @admin.register(models.Contact)
-class ContactADmin(admin.ModelAdmin):
+class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone',
-    ordering = 'id',
+    ordering = '-id',
     list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
     list_editable = 'first_name', 'last_name',
     list_display_links = 'phone','id',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id','name',
+    ordering = '-id',
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'name',
