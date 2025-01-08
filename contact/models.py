abdefa49cb2a1c1769302065589d4dde9d490e97
crispimUchoa <francisco,crispim@aluno.uece.br>
2025-01-08 19:28:32 -0300

    create owner atributte in contact model

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     class Meta:
@@ -26,6 +27,11 @@ class Contact(models.Model):
         on_delete=models.SET_NULL,
         blank=True, null=True        
         )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
