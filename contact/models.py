22cb5f1a9016cca61cf3e85c05580b856af2852b
crispimUchoa <francisco,crispim@aluno.uece.br>
2025-01-08 19:11:50 -0300

    fix plural of category model

--- contact/models.py
+++ contact/models.py
@@ -2,8 +2,12 @@ from django.db import models
 from django.utils import timezone
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
     name = models.CharField(max_length=50)
-
+    
     def __str__(self):
         return self.name
 
