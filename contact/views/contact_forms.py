e92f92d02939b8615cdbecdb7896b9549deacc8e
crispimUchoa <francisco,crispim@aluno.uece.br>
2025-01-09 10:11:42 -0300

    create view contact.create

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def create(request):
+    context = {
+
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
